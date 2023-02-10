3d3efa95cf58277111508366c6bec8030fa1415c
unhugly <fanroge@gmail.com>
2023-02-10 15:44:47 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DBGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateDatabase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateDatabase
+            // 
+            this.btnCreateDatabase.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreateDatabase.Location = new System.Drawing.Point(268, 176);
+            this.btnCreateDatabase.Name = "btnCreateDatabase";
+            this.btnCreateDatabase.Size = new System.Drawing.Size(104, 23);
+            this.btnCreateDatabase.TabIndex = 0;
+            this.btnCreateDatabase.Text = "Create Database";
+            this.btnCreateDatabase.UseVisualStyleBackColor = true;
+            this.btnCreateDatabase.Click += new System.EventHandler(this.btnCreateDatabase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.btnCreateDatabase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateDatabase;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateDatabase_Click(object sender, EventArgs e)
+        {
+            SqlConnection myConn = new SqlConnection($"Server={Environment.MachineName}\\SQLEXPRESS;Integrated security=SSPI;database=master");
+
+            String str = "CREATE DATABASE MyDatabase ON PRIMARY " +
+             "(NAME = MyDatabase_Data, " +
+             "FILENAME = 'E:\\MyDatabaseData.mdf', " +
+             "SIZE = 2MB, MAXSIZE = 10MB, FILEGROWTH = 10%)" +
+             "LOG ON (NAME = MyDatabase_Log, " +
+             "FILENAME = 'E:\\MyDatabaseLog.ldf', " +
+             "SIZE = 1MB, " +
+             "MAXSIZE = 5MB, " +
+             "FILEGROWTH = 10%)";
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+                MessageBox.Show("DataBase is Created Successfully", "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                if (myConn.State == ConnectionState.Open)
+                {
+                    myConn.Close();
+                }
+            }
+        }
+    }
+}
